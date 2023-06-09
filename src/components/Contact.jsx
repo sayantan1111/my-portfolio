cd5d31e25fb6fc6a212bf8a2a1f10fd139ab498a
sayantan1111 <108960197+sayantan1111@users.noreply.github.com>
2023-06-09 21:38:50 +0530

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -10,7 +10,7 @@ const Contact = () => {
             </div>
             <div className='flex justify-center items-center'>
                 <form action="https://getform.io/f/61d6a2c5-a12b-4468-a817-baf4c926e927" method='POST' className='flex flex-col w-full md:w-1/2'>
-                    <input type='text' name='name' placeholder='Enter your name' className='p-2 bg-transparent border-2 rounded-md text-white focus:outline-none'/>
+                    <input type='text' name='name' placeholder='Enter your name' className='my-4 p-2 bg-transparent border-2 rounded-md text-white focus:outline-none'/>
                     <input type='text' name='email' placeholder='Enter your email' className='my-4 p-2 bg-transparent border-2 rounded-md text-white focus:outline-none'/>
                         <input type='number' name='mobilenumber' placeholder='Enter your mobile number' className='my-4 p-2 bg-transparent border-2 rounded-md text-white focus:outline-none'/>
 
