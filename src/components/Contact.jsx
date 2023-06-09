26de6229884a8af7d019e958b31246f959d234be
sayantan1111 <108960197+sayantan1111@users.noreply.github.com>
2023-06-09 21:29:05 +0530

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -12,6 +12,8 @@ const Contact = () => {
                 <form action="https://getform.io/f/61d6a2c5-a12b-4468-a817-baf4c926e927" method='POST' className='flex flex-col w-full md:w-1/2'>
                     <input type='text' name='name' placeholder='Enter your name' className='p-2 bg-transparent border-2 rounded-md text-white focus:outline-none'/>
                     <input type='text' name='email' placeholder='Enter your email' className='my-4 p-2 bg-transparent border-2 rounded-md text-white focus:outline-none'/>
+                        <input type='number' name='mobilenumber' placeholder='Enter your mobile number' className='my-4 p-2 bg-transparent border-2 rounded-md text-white focus:outline-none'/>
+
 <textarea name='message' placeholder='Enter your message' rows='10' className='p-2 bg-transparent border-2 rounded-md text-white focus:outline-none'></textarea>
 <button className='text-white bg-gradient-to-b from-cyan-500 to-blue-500 px-6 py-3 my-8 mx-auto flex items-center rounded-md hover:scale-110 duration-300'>Let's Talk</button>
                 </form>
