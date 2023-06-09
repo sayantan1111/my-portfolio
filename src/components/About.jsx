40045703f36f97f241ab743ca93c07a8fbdced89
Sayantan Mandal <sayantanmandal253@gmail.com>
2023-06-09 20:50:46 +0530

    project done

---
+++ src/components/About.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div name="about" className='w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white'>
+      <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
+        <div className='pb-8'>
+
+        <p className='text-4xl font-bold inline border-b-4 border-gray-500'>
+          About
+        </p>
+        </div>
+        <p className='text-xl-mt-20'>I am the student of SRM University , B-Tech in Computer Science, CHENNAI-TamilNadu. I an currently doing my DSA course from GEEKS FOR GEEKS. And also certified frontend web Developer.
+</p>
+    <br/>
+    <p className='text-xl'>Hi, I'm Sayantan, an aspiring web developer with a passion for creating captivating online experiences.
+
+With a keen eye for design and a love for coding, I strive to combine aesthetics and functionality to deliver websites that not only look stunning but also provide seamless user experiences. From eye-catching layouts to intuitive navigation, I understand the importance of engaging visitors and keeping them hooked.Well, as a web development student, I am constantly pushing the boundaries of my skills and staying up-to-date with the latest industry trends and technologies. Whether it's crafting responsive designs that adapt flawlessly to any device or incorporating interactive elements to make your website truly immersive, I have the expertise to make it happen.</p>
+      </div>
+    </div>
+  )
+}
+
+export default About
---
+++ src/components/Home.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import HeroImage from "../assets/heroImage.jpg"
+import {MdOutlineKeyboardArrowRight} from "react-icons/md"
+import {Link} from "react-scroll"
+const Home = () => {
+  return (
+    <div name="home" className='h-screen w-full bg-gradient-to-b from-black to-gray-800 text-white'>
+<div className='max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row'>
+
+    <div className='flex flex-col justify-center h-full'>
+        <h2 className='text-4xl sm:text-7xl font-bold text-white '>I'm a FrontEnd Developer</h2>
+        <p className='text-gray-500 py-4 max-w-md' >I'm a Web Developer, I use programming languages like HTML, CSS, and JavaScript to create websites that are functional, visually appealing, and easy to use.</p>
+        <div>
+            <Link to="portfolio" smooth duration={500} className='text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer'>Portfolio
+<span className='group-hover:rotate-90 duration-300'>
+            <MdOutlineKeyboardArrowRight size={25}/>
+</span>
+            </Link>
+        </div>
+    </div>
+
+    <div>
+        <img src={HeroImage} alt="my profile" className='rounded-2xl mx-auto w-2/3 md:w-half'></img>
+    </div>
+</div>
+
+    </div>
+  )
+};
+
+export default Home
